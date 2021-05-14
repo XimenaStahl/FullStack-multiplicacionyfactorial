8bfe60af0bdab8224bff9dc4fc85c5f78970881d
unknown <ximenastahl@gmail.com>
2021-05-13 20:18:18 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+// Solicitar un número al usuario
+const num = parseInt(prompt('Ingrese un número entre 1 y 20'));
+
+// Función Multiplicación
+const multiplicar = (num) => {
+    let factorial = 1
+    for (let i = 1; i <= num; i++) {
+
+        // multiplicación
+        let multip = i * num;
+        console.log(`${i} x ${num} = ${multip}`)
+    }
+};
+// Función Factorial
+const factorial = (num) => {
+
+    for (let i = 1; i <= num; i++) {
+        // factorial
+        let factor = 1
+        for (let j = 1; j <= i; j++) {
+            factor = factor * j;
+        }
+        console.log(`${i}! = ${factor}`)
+    }
+};
+
+// Validar si el usuario ingresó un número entre 1 y 20
+if (num >= 1 && num <= 20) {
+    // cumple la condición así que continúa llamando la función
+    multiplicar(num);
+    factorial(num);
+} else {
+    alert("número ingresado fuera de rango")
+}
